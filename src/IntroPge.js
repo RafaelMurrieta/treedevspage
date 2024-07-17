ac833ea81ae2e02efa6821e9d587f3347073c56e
RafaelMurrieta <r7121005846@gmail.com>
2024-07-17 09:58:55 -0600

    Modificacion de diseño

--- src/IntroPge.js
+++ src/IntroPge.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './index.css'
 import TreeDevsIcon from './assets/treedevslogo.jpg'
 
 const IntroPge = () => {
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,8 @@ function Navbar() {
   return (
     <nav>
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#services">Services</a></li>
+        <li><a href="#home">Inicio</a></li>
+        <li><a href="#services">Servicios</a></li>
         <li><a href="#team">Team</a></li>
       </ul>
     </nav>
